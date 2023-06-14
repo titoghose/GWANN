1b12e6da9e4901e1e272c5c1861e4329746cabe4
titoghose <titoghose@gmail.com>
2023-06-14 17:05:03 +0100

    Added genes to sensitivity analysis 1

--- Code_AD/sensitivity_analyses.py
+++ Code_AD/sensitivity_analyses.py
@@ -13,12 +13,21 @@ def sensitivity_1(chroms:list):
     mat_df = pd.read_csv(f'{param_folder}/Annotated_NoDup_MatAD_NN_GWAS.csv')
     pat_df = pd.read_csv(f'{param_folder}/Annotated_NoDup_PatAD_NN_GWAS.csv')
     meta_df = pd.read_csv(f'{param_folder}/Annotated_AD_Meta.csv')
-    labels = {'PATERNAL_MARIONI':meta_df, 
-              'MATERNAL_MARIONI':meta_df}
+    
+    gdf = pd.read_csv('/home/upamanyu/GWANN/GWANN/datatables/gene_annot.csv')
+    marioni_meta_and_common_hits = ['CR1', 'PICALM', 'ZNF232', 'BIN1', 'NIFKP9', 
+                    'APP', 'PILRA', 'ZCWPW1', 'NYAP1', 'CLU', 'SIGLEC11', 
+                    'ABCA1', 'ABCA7']
+    marioni_df = gdf.loc[gdf['symbol'].isin(marioni_meta_and_common_hits)]
+    marioni_df.rename(columns={'symbol':'Gene', 'chrom':'Chrom'}, inplace=True)
+    marioni_df['P'] = 0
+
+    labels = {'PATERNAL_MARIONI':marioni_df, 
+              'MATERNAL_MARIONI':marioni_df}
     for label, df in labels.items():
-        hits = df.loc[df['P'] < (0.05/73310)].astype({'chrom':str})
+        hits = df.loc[df['P'] < (0.05/73310)].astype({'Chrom':str})
         print(label)
-        for chrom, idx in hits.groupby('chrom').groups.items():
+        for chrom, idx in hits.groupby('Chrom').groups.items():
             if not str(chrom) in chroms:
                 continue
             glist = hits.loc[idx]['Gene'].to_list()

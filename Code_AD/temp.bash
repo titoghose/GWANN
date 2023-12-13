56c4f0a76b08c7b9244042366ed2255d2f526663
titoghose <titoghose@gmail.com>
2023-12-13 18:23:39 +0000

    Modifications to requirements.txt

--- Code_AD/temp.bash
+++ Code_AD/temp.bash
@@ -9,16 +9,17 @@
 # done
 
 # seeds=(0 4250 937 89 172 37 363 281 142 7282)
-seed=142
-chroms=(21 19 17 15 13 11 9 7 5 3 1)
+seed=7282
+# chroms=(21 19 17 15 13 11 9 7 5 3 1)
+chroms=(22 20 18 16 14 12 10 8 6 4 2)
 group_size=10
 exp_name="Sens8_"$seed""$seed"_GS"$group_size"_v4"
 log_folder="/home/upamanyu/GWANN/Code_AD/NN_Logs/FH_AD_Chr"$exp_name"_GWANNet5_[32,16]_Dr_0.5_LR:0.005_BS:256_Optim:adam"
 
-export TORCH_SEED=$seed
-export GROUP_SEED=$seed
-export GROUP_SIZE=$group_size
-time python cov_model.py --label FH_AD > "./Runs/cov"$seed".txt" 2>&1
+# export TORCH_SEED=$seed
+# export GROUP_SEED=$seed
+# export GROUP_SIZE=$group_size
+# time python cov_model.py --label FH_AD > "./Runs/cov"$seed".txt" 2>&1
 
 for chrom in ${chroms[@]}
 do
